31a2056b14872126befa119bd098d4bc4424ea7e
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-08-28 20:03:55 +0300

    yapıldı

---
+++ src/main/java/com/workintech/api/entity/Burger.java
@@ -0,0 +1,42 @@
+package com.workintech.api.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="burger", schema = "spring")
+public class Burger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="name")
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @Column(name="price")
+    @DecimalMin("20")
+    private double price;
+
+    @Column(name="is_vegan")
+    private boolean isVegan;
+
+    @Enumerated(EnumType.STRING)
+    private BreadType breadType;
+
+    @Column(name="content")
+    private List<String> content;
+}
---
+++ src/main/java/com/workintech/api/exceptions/BurgerException.java
@@ -0,0 +1,12 @@
+package com.workintech.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class BurgerException extends RuntimeException{
+    private HttpStatus status;
+
+    public BurgerException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+}
